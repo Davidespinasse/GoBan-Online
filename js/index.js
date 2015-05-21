9225686ecf03c0a2ad3c51fd061c8c2579c0325f
Guillaume Lebelt <guillaumelebelt@gmail.com>
2015-05-21 14:54:06 +0200

    Header site
    Create the first try to header, with Facebook share button, connexion,
    title of the game and hamburger for open a navigation.

---
+++ js/index.js
@@ -0,0 +1,69 @@
+var compteur_second = 0;
+
+$('#container_hamburger').on('click', function() {
+	if (compteur_second == 0) {
+		$('#hamburger_2').css('opacity', '0')
+		$('#hamburger_1').css({'top' : '14px'})
+		$('#hamburger_3').css({'top' : '14px'})
+		setTimeout(function() {
+			compteur_second++;
+		},100)
+		setTimeout(function() {
+			$('#hamburger_1').css({'transform' : 'rotateZ(45deg)'})
+			$('#hamburger_3').css({'transform' : 'rotateZ(-45deg)'})
+		},300)
+	}
+	if (compteur_second == 1) {
+		$('#hamburger_2').css('opacity', '1')
+		$('#hamburger_1').css({'transform' : 'rotateZ(0deg)'})
+		$('#hamburger_3').css({'transform' : 'rotateZ(0deg)'})
+		setTimeout(function() {
+			compteur_second = 0;
+		},100)
+		setTimeout(function() {
+			$('#hamburger_1').css({'top' : '7px'})
+			$('#hamburger_3').css({'top' : '21px'})
+		},300)
+	}
+});
+
+(function() {
+	var triggerBttn = document.getElementById( 'trigger-overlay' ),
+		overlay = document.querySelector( 'div.overlay' ),
+		closeBttn = overlay.querySelector( 'button.overlay-close' );
+		transEndEventNames = {
+			'WebkitTransition': 'webkitTransitionEnd',
+			'MozTransition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'msTransition': 'MSTransitionEnd',
+			'transition': 'transitionend'
+		},
+		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+		support = { transitions : Modernizr.csstransitions };
+
+	function toggleOverlay() {
+		if( classie.has( overlay, 'open' ) ) {
+			classie.remove( overlay, 'open' );
+			classie.add( overlay, 'close' );
+			var onEndTransitionFn = function( ev ) {
+				if( support.transitions ) {
+					if( ev.propertyName !== 'visibility' ) return;
+					this.removeEventListener( transEndEventName, onEndTransitionFn );
+				}
+				classie.remove( overlay, 'close' );
+			};
+			if( support.transitions ) {
+				overlay.addEventListener( transEndEventName, onEndTransitionFn );
+			}
+			else {
+				onEndTransitionFn();
+			}
+		}
+		else if( !classie.has( overlay, 'close' ) ) {
+			classie.add( overlay, 'open' );
+		}
+	}
+
+	triggerBttn.addEventListener( 'click', toggleOverlay );
+	closeBttn.addEventListener( 'click', toggleOverlay );
+})();
